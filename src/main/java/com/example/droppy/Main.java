ecb5bacdb9ee573b565e70d701efdf16be50c89c
c0nfusedf0x <veronika.baskovska.1.2.3@gmail.com>
2025-10-17 19:42:33 +0200

    changed main view

--- src/main/java/com/example/droppy/Main.java
+++ src/main/java/com/example/droppy/Main.java
@@ -1,23 +1,35 @@
 package com.example.droppy;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
     @Override
-    public void start(Stage stage) {
-        Button btn = new Button("Click me");
-        btn.setOnAction(e -> btn.setText("Hello, Droppy!"));
+    public void start(Stage stage) throws Exception {
+//        Button btn = new Button("Click me");
+//        btn.setOnAction(e -> btn.setText("Hello, Droppy!"));
+//
+//        StackPane root = new StackPane(btn);
+//        Scene scene = new Scene(root, 400, 300);
+//
+//        stage.setTitle("Droppy");
+//        stage.setScene(scene);
+//        stage.show();
+        var loader = new FXMLLoader(getClass().getResource("/LoginView.fxml"));
+        Parent rootPane = loader.load();
 
-        StackPane root = new StackPane(btn);
-        Scene scene = new Scene(root, 400, 300);
-
-        stage.setTitle("Droppy");
+        var scene = new Scene(rootPane);
+        stage.setTitle("Log in");
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
