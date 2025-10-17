a2cc021a979b312bd7257e4c02dfcbca44658df7
c0nfusedf0x <veronika.baskovska.1.2.3@gmail.com>
2025-10-17 21:54:26 +0200

    add back button

--- src/main/java/com/example/droppy/controller/SignInController.java
+++ src/main/java/com/example/droppy/controller/SignInController.java
@@ -1,12 +1,22 @@
 package com.example.droppy.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 
 public class SignInController {
+
+    @FXML
+    private Button backButton;
 
     @FXML
     private TextField comfirmPasswordTextField;
@@ -28,5 +38,18 @@ public class SignInController {
 
     @FXML
     private TextField surnameTextField;
+
+    @FXML
+    void onBackButtonClick(ActionEvent event) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/LoginView.fxml"));
+        Parent rootPane = loader.load();
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        var scene = new Scene(rootPane);
+        stage.setTitle("Droppy");
+        stage.setScene(scene);
+        stage.show();
+    }
 
 }
