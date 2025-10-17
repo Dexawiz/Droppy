460fe94b8b38299d6551991c6a3865b4445f84bb
dexawiz <nazarp118@gmail.com>
2025-10-17 21:08:09 +0200

    button to sign in window is now working

--- src/main/java/com/example/droppy/controller/LoginController.java
+++ src/main/java/com/example/droppy/controller/LoginController.java
@@ -1,10 +1,19 @@
 package com.example.droppy.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
+import javafx.event.ActionEvent;
+import java.io.IOException;
+
+@SuppressWarnings("unused")
 public class LoginController {
 
     @FXML
@@ -25,4 +34,16 @@ public class LoginController {
     @FXML
     private Button signinButton;
 
+    @FXML
+    void onSigninButtonClick(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/SignInView.fxml"));
+        Parent rootPane = loader.load();
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        var scene = new Scene(rootPane);
+        stage.setTitle("Droppy");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
--- src/main/java/com/example/droppy/controller/SignInController.java
+++ src/main/java/com/example/droppy/controller/SignInController.java
@@ -5,6 +5,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
+
 public class SignInController {
 
     @FXML
