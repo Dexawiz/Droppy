0996b96a1e22807cd80e4472bec72248c34ba16b
c0nfusedf0x <veronika.baskovska.1.2.3@gmail.com>
2025-10-17 19:18:47 +0200

    added SignIn

---
+++ src/main/java/com/example/droppy/controller/SignInController.java
@@ -0,0 +1,31 @@
+package com.example.droppy.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class SignInController {
+
+    @FXML
+    private TextField comfirmPasswordTextField;
+
+    @FXML
+    private TextField emailTextField;
+
+    @FXML
+    private TextField nameTextField;
+
+    @FXML
+    private TextField passwordTextField;
+
+    @FXML
+    private Button signInButton;
+
+    @FXML
+    private Text signInText;
+
+    @FXML
+    private TextField surnameTextField;
+
+}
