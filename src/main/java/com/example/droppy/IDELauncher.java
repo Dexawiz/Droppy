69a4f885f626133930e8015b62e6522712da3ef8
dexawiz <nazarp118@gmail.com>
2025-10-17 09:27:09 +0200

    basic app, installed javafx

---
+++ src/main/java/com/example/droppy/IDELauncher.java
@@ -0,0 +1,9 @@
+package com.example.droppy;
+
+public class IDELauncher {
+
+    // This main method is used for running the app from IntelliJ IDEA.
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
---
+++ src/main/java/com/example/droppy/Main.java
@@ -0,0 +1,27 @@
+package com.example.droppy;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) {
+        Button btn = new Button("Click me");
+        btn.setOnAction(e -> btn.setText("Hello, Droppy!"));
+
+        StackPane root = new StackPane(btn);
+        Scene scene = new Scene(root, 400, 300);
+
+        stage.setTitle("Droppy");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
