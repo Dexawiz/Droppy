e6e87b26fdda754c6bf38e43d158293aa196fb32
c0nfusedf0x <veronika.baskovska.1.2.3@gmail.com>
2025-10-26 18:23:44 +0100

    HomeView and CompanyComponent added

---
+++ src/main/java/com/example/droppy/controller/CompanyComponentController.java
@@ -0,0 +1,19 @@
+package com.example.droppy.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+
+public class CompanyComponentController {
+
+    @FXML
+    private TextArea companyDescriptionTextArea;
+
+    @FXML
+    private ImageView companyImageView;
+
+    @FXML
+    private Label companyNameLabel;
+
+}
---
+++ src/main/java/com/example/droppy/controller/HomeController.java
@@ -0,0 +1,35 @@
+package com.example.droppy.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.shape.Circle;
+
+public class HomeController {
+
+    @FXML
+    private Circle avatarSmall;
+
+    @FXML
+    private Label categoriesLabel;
+
+    @FXML
+    private CheckBox categoryItemCheckBox;
+
+    @FXML
+    private Label droppyTextLogo;
+
+    @FXML
+    private Label restaurantsLabel;
+
+    @FXML
+    private TextField searchTextField;
+
+    @FXML
+    void onCategoryItemCheckBoxClicked(ActionEvent event) {
+
+    }
+
+}
