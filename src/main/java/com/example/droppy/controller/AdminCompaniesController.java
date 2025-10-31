fdb22f7e6931487c2489291e5f61d259cafbda8c
c0nfusedf0x <veronika.baskovska.1.2.3@gmail.com>
2025-10-31 16:46:42 +0100

    added Admin Companies+Drivers views+controllers

---
+++ src/main/java/com/example/droppy/controller/AdminCompaniesController.java
@@ -0,0 +1,55 @@
+package com.example.droppy.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
+
+public class AdminCompaniesController {
+
+    @FXML
+    private Button addCompanyButton;
+
+    @FXML
+    private Button deleteCompanyButton;
+
+    @FXML
+    private ListView<?> driversListView;
+
+    @FXML
+    private Label droppyTextLogo;
+
+    @FXML
+    private Button logOutButton;
+
+    @FXML
+    private Button switchToDriversButton;
+
+    @FXML
+    void addCompanyButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void attendancesListViewMouseClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void deleteCompanyButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onDriversSwitchButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onLogOutButtonClicked(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/com/example/droppy/controller/AdminDriversController.java
@@ -0,0 +1,55 @@
+package com.example.droppy.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
+
+public class AdminDriversController {
+
+    @FXML
+    private Button addDriverButton;
+
+    @FXML
+    private Button deleteDriverButton;
+
+    @FXML
+    private ListView<?> driversListView;
+
+    @FXML
+    private Label droppyTextLogo;
+
+    @FXML
+    private Button logOutButton;
+
+    @FXML
+    private Button switchToCompaniesButton;
+
+    @FXML
+    void addDriverButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void attendancesListViewMouseClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void deleteDriverButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onCompaniesSwitchButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onLogOutButtonClicked(ActionEvent event) {
+
+    }
+
+}
