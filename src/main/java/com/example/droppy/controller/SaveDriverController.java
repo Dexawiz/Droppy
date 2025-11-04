126b20cd1e2330c6c068e6966fd2e111caee41ed
c0nfusedf0x <veronika.baskovska.1.2.3@gmail.com>
2025-11-05 00:58:44 +0100

    saveDriverView added

---
+++ src/main/java/com/example/droppy/controller/SaveDriverController.java
@@ -0,0 +1,45 @@
+package com.example.droppy.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class SaveDriverController {
+
+    @FXML
+    private Button deleteDriverButton;
+
+    @FXML
+    private Label driverLabel;
+
+    @FXML
+    private TextField driverNameTextField;
+
+    @FXML
+    private TextField driverPasswordField;
+
+    @FXML
+    private TextField driverPhoneNumberTextField;
+
+    @FXML
+    private TextField drverSurnameTextField;
+
+    @FXML
+    private TextField emailDriverTextField;
+
+    @FXML
+    private Button saveDriverButton;
+
+    @FXML
+    void onDeleteDriverButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSaveDriverButtonClick(ActionEvent event) {
+
+    }
+
+}
