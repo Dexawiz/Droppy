345130daeec8736b598dc83264fec620dae1ea48
c0nfusedf0x <veronika.baskovska.1.2.3@gmail.com>
2025-11-09 16:46:55 +0100

    SaveCompany fixed

---
+++ src/main/java/com/example/droppy/controller/SaveCompanyController.java
@@ -0,0 +1,87 @@
+package com.example.droppy.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class SaveCompanyController {
+
+    @FXML
+    private Button addFoodButton;
+
+    @FXML
+    private Button backtoCompaniesButton;
+
+    @FXML
+    private ChoiceBox<?> categoryCompanyChoiceBox;
+
+    @FXML
+    private ComboBox<?> closingHourCBox;
+
+    @FXML
+    private ComboBox<?> closingMinuteCBox;
+
+    @FXML
+    private TextField companyAddressTextField;
+
+    @FXML
+    private Label companyLabel;
+
+    @FXML
+    private TextField companyNameTextField;
+
+    @FXML
+    private Button deleteCompanyButton;
+
+    @FXML
+    private Button deleteFoodButton;
+
+    @FXML
+    private Label foodLabel;
+
+    @FXML
+    private ListView<?> foodListView;
+
+    @FXML
+    private ComboBox<?> openingHourCBox;
+
+    @FXML
+    private ComboBox<?> openingMinuteCBox;
+
+    @FXML
+    private TextField phoneNumberCompanyTextField;
+
+    @FXML
+    private Button saveCompanyButton;
+
+    @FXML
+    void onAddButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onBackToCompaniesButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onDeleteCompanyButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onDeleteFoodButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSaveBCompanyButtonClick(ActionEvent event) {
+
+    }
+
+}
