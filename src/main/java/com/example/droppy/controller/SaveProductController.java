74d2167347b265735060509bea00aad46faf4f8c
c0nfusedf0x <veronika.baskovska.1.2.3@gmail.com>
2025-11-09 19:03:33 +0100

    SaveProduct + controller added

---
+++ src/main/java/com/example/droppy/controller/SaveProductController.java
@@ -0,0 +1,51 @@
+package com.example.droppy.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class SaveProductController {
+
+    @FXML
+    private Label EURLabel;
+
+    @FXML
+    private Button backtoCompanyButton;
+
+    @FXML
+    private Button deleteProductButton;
+
+    @FXML
+    private TextArea descProductTextField;
+
+    @FXML
+    private TextField nameProductTextField;
+
+    @FXML
+    private TextField priceProductTextField;
+
+    @FXML
+    private Label productLabel;
+
+    @FXML
+    private Button saveProductButton;
+
+    @FXML
+    void onBackToCompanyButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onDeleteProductButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSaveProductButtonClick(ActionEvent event) {
+
+    }
+
+}
