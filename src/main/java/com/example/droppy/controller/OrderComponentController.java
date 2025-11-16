a7f0c77b669b363dbd74c01efd2367a40371ba4b
c0nfusedf0x <veronika.baskovska.1.2.3@gmail.com>
2025-11-16 18:54:21 +0100

    OrderComponent

---
+++ src/main/java/com/example/droppy/controller/OrderComponentController.java
@@ -0,0 +1,57 @@
+package com.example.droppy.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class OrderComponentController {
+
+    @FXML
+    private Label CompanyLabel;
+
+    @FXML
+    private Label EURLabel;
+
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private Label addressCompanyDemo;
+
+    @FXML
+    private Label addressToDeliverDemo;
+
+    @FXML
+    private Label addressToDeliverLabel;
+
+    @FXML
+    private Label companyAddressLabel;
+
+    @FXML
+    private Label nameCompanyDemo;
+
+    @FXML
+    private Label orderIDDemo;
+
+    @FXML
+    private Label orderIDLabel;
+
+    @FXML
+    private Label priceDemo;
+
+    @FXML
+    private Label priceLabel;
+
+    @FXML
+    private Label productToDeliverDemo;
+
+    @FXML
+    private Label productToDeliverLabel;
+
+    @FXML
+    void onAddButtonClick(ActionEvent event) {
+
+    }
+
+}
