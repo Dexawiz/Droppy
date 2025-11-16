7ba926a651fffae4536bf29ccb4c48de6b8d2ea2
c0nfusedf0x <veronika.baskovska.1.2.3@gmail.com>
2025-11-16 17:44:26 +0100

    DriverViews

---
+++ src/main/java/com/example/droppy/controller/DriverDeliveredOrderController.java
@@ -0,0 +1,70 @@
+package com.example.droppy.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class DriverDeliveredOrderController {
+
+    @FXML
+    private Button AcOrderButton;
+
+    @FXML
+    private Button AvOrderButton;
+
+    @FXML
+    private Button DeOrderButton;
+
+    @FXML
+    private VBox column1VBOX;
+
+    @FXML
+    private Label deliveredOrderLabel;
+
+    @FXML
+    private ChoiceBox<?> driverStatusCB;
+
+    @FXML
+    private Label droppyTextLogo;
+
+    @FXML
+    private HBox header;
+
+    @FXML
+    private Button logOutButton;
+
+    @FXML
+    private Label nameDriverLabel;
+
+    @FXML
+    private ScrollPane scrollPane;
+
+    @FXML
+    private Label surnameDriverLabel;
+
+    @FXML
+    void onAcOrderButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onAvOrderButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onDeOrderButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onLogOutButtonClicked(ActionEvent event) {
+
+    }
+
+}
