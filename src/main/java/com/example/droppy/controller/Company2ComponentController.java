54a541ee8cea6d44fdbe65da97edfa126bf5f27b
c0nfusedf0x <veronika.baskovska.1.2.3@gmail.com>
2025-11-16 19:51:36 +0100

    OrderComponent2

---
+++ src/main/java/com/example/droppy/controller/Company2ComponentController.java
@@ -0,0 +1,62 @@
+package com.example.droppy.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+public class Company2ComponentController {
+    @FXML
+    private VBox mainPane;
+    @FXML
+    private Label CompanyLabel;
+
+    @FXML
+    private Label EURLabel;
+
+    @FXML
+    private Label addressCompanyDemo;
+
+    @FXML
+    private Label addressToDeliverDemo;
+
+    @FXML
+    private Label addressToDeliverLabel;
+
+    @FXML
+    private Label companyAddressLabel;
+
+    @FXML
+    private Label driverLabel;
+
+    @FXML
+    private Label driverNameDemo;
+
+    @FXML
+    private Label driverSurnameDemo;
+
+    @FXML
+    private Label nameCompanyDemo;
+
+    @FXML
+    private Label orderIDDemo;
+
+    @FXML
+    private Label orderIDLabel;
+
+    @FXML
+    private Label priceDemo;
+
+    @FXML
+    private Label priceLabel;
+
+    @FXML
+    private Label productToDeliverDemo;
+
+    @FXML
+    private Label productToDeliverLabel;
+
+    @FXML
+    private ChoiceBox<?> statusOfDelivery;
+
+}
