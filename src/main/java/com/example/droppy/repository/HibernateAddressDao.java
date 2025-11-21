422f8fc253acaee4ea2166c0a8d15507608736ba
dexawiz <nazarp118@gmail.com>
2025-11-21 21:12:34 +0100

    some fixes

--- src/main/java/com/example/droppy/repository/HibernateAddressDao.java
+++ src/main/java/com/example/droppy/repository/HibernateAddressDao.java
@@ -1,6 +1,7 @@
 package com.example.droppy.repository;
 
 import com.example.droppy.domain.entity.Address;
+import com.example.droppy.domain.entity.User;
 import com.example.droppy.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -39,10 +40,15 @@ public class HibernateAddressDao  implements AddressDao {
 
     @Override
     public List<Address> findByUserId(Long userId) {
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            return session.createQuery("FROM Address WHERE user.id = :userId", Address.class)
+        try(Session session = HibernateUtil.getSessionFactory().openSession()){
+            User user = session.createQuery("FROM User WHERE id = :userId", User.class)
                     .setParameter("userId", userId)
-                    .list();
+                    .uniqueResult();
+            if(user != null && user.getAddress() != null){
+                return List.of(user.getAddress());
+            } else {
+                return List.of();
+            }
         }
     }
 
