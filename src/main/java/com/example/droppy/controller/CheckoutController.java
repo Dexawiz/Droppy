9925abfab091ecd44bf7d280b96e8e39c593203a
c0nfusedf0x <veronika.baskovska.1.2.3@gmail.com>
2025-11-22 20:33:24 +0100

    Checkout added

---
+++ src/main/java/com/example/droppy/controller/CheckoutController.java
@@ -0,0 +1,29 @@
+package com.example.droppy.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Circle;
+import org.kordamp.ikonli.javafx.FontIcon;
+
+public class CheckoutController {
+
+    @FXML
+    private Circle avatarSmall;
+
+    @FXML
+    private Label droppyTextLogo;
+
+    @FXML
+    private TextField searchTextField;
+
+    @FXML
+    private FontIcon shoppingCartIcon;
+
+    @FXML
+    void onProfileClick(MouseEvent event) {
+
+    }
+
+}
