3072d5bd9bfca904c5fb17b5cf98a1598ad70906
c0nfusedf0x <veronika.baskovska.1.2.3@gmail.com>
2025-11-26 15:35:19 +0100

    Checkout Item added

---
+++ src/main/java/com/example/droppy/controller/ItemCheckoutController.java
@@ -0,0 +1,48 @@
+package com.example.droppy.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+
+public class ItemCheckoutController {
+
+    @FXML
+    private Label ItemNameLabel;
+
+    @FXML
+    private Button addItemButton;
+
+    @FXML
+    private Text decriptionText;
+
+    @FXML
+    private Button deleteItemButton;
+
+    @FXML
+    private Label eurLabel;
+
+    @FXML
+    private Label numberOfItemsLabel;
+
+    @FXML
+    private Label priceDemoLabel;
+
+    @FXML
+    private Label priceLabel;
+
+    @FXML
+    private Label xLabel;
+
+    @FXML
+    void onAddItemLabelButtonCLick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onDeleteItemButtonClick(ActionEvent event) {
+
+    }
+
+}
