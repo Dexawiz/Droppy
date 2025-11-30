c4737e993b58eb2eb00395244170ec98b461c6c3
c0nfusedf0x <veronika.baskovska.1.2.3@gmail.com>
2025-11-30 21:07:15 +0100

    profile adding/editing/deleting card DONE

--- src/main/java/com/example/droppy/controller/ProfileController.java
+++ src/main/java/com/example/droppy/controller/ProfileController.java
@@ -3,6 +3,7 @@ package com.example.droppy.controller;
 import com.example.droppy.domain.enums.Language;
 import com.example.droppy.repository.HibernateUserDao;
 import com.example.droppy.service.AuthService;
+import com.example.droppy.service.I18n;
 import com.example.droppy.service.Session;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -17,6 +18,8 @@ import javafx.scene.text.Text;
 import com.example.droppy.domain.entity.User;
 import javafx.stage.Stage;
 
+import java.util.Locale;
+
 public class ProfileController {
 
     private AuthService authService;
@@ -26,6 +29,10 @@ public class ProfileController {
     public void init(AuthService authService, Stage mainStage) {
         this.authService = authService;
         this.mainStage = mainStage;
+    }
+
+    public void init(AuthService authService) {
+        this.authService = authService;
     }
 
     @FXML
@@ -106,14 +113,109 @@ public class ProfileController {
     @FXML
     private TextField phoneField;
 
+    //pre kartu
     @FXML
-    private ChoiceBox<Language> languageCB;
+    private TextField cardNumberTF;
+
+    @FXML
+    private TextField monthTF;
+
+    @FXML
+    private TextField yearTF;
+
+    @FXML
+    private PasswordField CCVTF;
+
+    @FXML
+    private Label noCardLabel;
+
+    @FXML
+    private Button addEditCardButton;
+
+    @FXML
+    private Button deleteCardButton;
+
+
+    @FXML
+    private ChoiceBox<Language> languageCB1;
 
     private boolean isEditing = false;
 
-    @FXML
-    void onAddCreditDebitCardButtonClick(ActionEvent event) {
+    private boolean isAddingEditingCard = false;
 
+    @FXML
+    void onAddEditCardButtonCLick(ActionEvent event) {
+        User user = Session.getLoggedUser();
+
+        if (!isAddingEditingCard) {
+            isAddingEditingCard = true;
+            addEditCardButton.setText("Save");
+
+            if (user != null && user.getCardNumber() != null) {
+                String[] parts = user.getCardNumber().split("/");
+                if (parts.length == 4) {
+                    cardNumberTF.setText(parts[0]);
+                    monthTF.setText(parts[1]);
+                    yearTF.setText(parts[2]);
+                    CCVTF.setText(parts[3]);
+                }
+            } else {
+                cardNumberTF.clear();
+                monthTF.clear();
+                yearTF.clear();
+                CCVTF.clear();
+            }
+
+
+            cardNumberTF.setVisible(true);
+            cardNumberTF.setManaged(true);
+            monthTF.setVisible(true);
+            monthTF.setManaged(true);
+            yearTF.setVisible(true);
+            yearTF.setManaged(true);
+            CCVTF.setVisible(true);
+            CCVTF.setManaged(true);
+
+            // skry label
+            noCardLabel.setVisible(false);
+            noCardLabel.setManaged(false);
+        } else {
+            // uloženie do DB
+            String cardNumber = cardNumberTF.getText().trim();
+            String month = monthTF.getText().trim();
+            String year = yearTF.getText().trim();
+            String ccv = CCVTF.getText().trim();
+
+            if (!cardNumber.isEmpty() && !month.isEmpty() && !year.isEmpty()) {
+                String card = cardNumber + "/" + month + "/" + year + "/" + ccv;
+
+                //pridanie do db
+                if (user != null) {
+                    user.setCardNumber(card);
+                    HibernateUserDao userDao = new HibernateUserDao();
+                    userDao.save(user);
+                }
+                loadUserData();
+                // maskovanie čísla karty
+                noCardLabel.setText("****************" + "/" + "**" + "/" + "**");
+                noCardLabel.setVisible(true);
+                noCardLabel.setManaged(true);
+
+                // skry text fields
+                cardNumberTF.setVisible(false);
+                cardNumberTF.setManaged(false);
+                monthTF.setVisible(false);
+                monthTF.setManaged(false);
+                yearTF.setVisible(false);
+                yearTF.setManaged(false);
+                CCVTF.setVisible(false);
+                CCVTF.setManaged(false);
+
+                addEditCardButton.setText("Edit");
+                isAddingEditingCard = false;
+
+            }
+        }
     }
 
     @FXML
@@ -146,14 +248,10 @@ public class ProfileController {
         }
     }
 
-    @FXML
-    void onEditCreditDebitCardButtonClick(ActionEvent event) {
-
-    }
 
     @FXML
     void onEditProfileButtonClick(ActionEvent event) {
-        if(!isEditing){
+        if (!isEditing) {
             isEditing = true;
             editProfileButton.setText("Save");
 
@@ -177,7 +275,7 @@ public class ProfileController {
             surnameField.setManaged(true);
             phoneField.setVisible(true);
             phoneField.setManaged(true);
-        }else{
+        } else {
             isEditing = false;
             editProfileButton.setText("Edit");
 
@@ -230,8 +328,19 @@ public class ProfileController {
         }
     }
 
-    public void init(AuthService authService) {
-        this.authService = authService;
+    @FXML
+    void onDeleteCardButtonClick(ActionEvent event) {
+        User loggedUser = Session.getLoggedUser();
+        if (loggedUser != null && loggedUser.getCardNumber() != null && !loggedUser.getCardNumber().isEmpty()) {
+            var alert = new Alert(Alert.AlertType.CONFIRMATION, "Do you want to delete your card?");
+            var result = alert.showAndWait();
+            if ((result.get() == javafx.scene.control.ButtonType.OK)) {
+                loggedUser.setCardNumber(null);
+                HibernateUserDao userDao = new HibernateUserDao();
+                userDao.save(loggedUser);
+            }
+        }
+        loadUserData();
     }
 
     @FXML
@@ -247,7 +356,8 @@ public class ProfileController {
 
             LoginController loginController = loader.getController();
             if (this.authService != null) {
-                loginController.init(this.authService, () -> {});
+                loginController.init(this.authService, () -> {
+                });
             }
 
             Scene scene = new Scene(rootPane);
@@ -257,18 +367,45 @@ public class ProfileController {
         }
     }
 
+
     @FXML
     private void initialize() {
-        languageCB.getItems().setAll(Language.values());
-        languageCB.setValue(Session.getCurrentLanguage());
+        //zmena jazyka
+        languageCB1.getItems().setAll(Language.values());
+        languageCB1.setValue(Session.getCurrentLanguage());
 
-        languageCB.valueProperty().addListener((obs, oldVal, newVal) -> {
+        I18n.setLocale(new Locale(Session.getCurrentLanguage().getCode()));
+        updateText();
+
+        languageCB1.valueProperty().addListener((obs, oldVal, newVal) -> {
             if (newVal != null) {
                 Session.setCurrentLanguage(newVal);
+                I18n.setLocale(new Locale(newVal.getCode()));
+                updateText();
             }
         });
 
+        //limit znakov na textove polia
+        cardNumberTF.setTextFormatter(new TextFormatter<String>(change ->
+                change.getControlNewText().length() <= 16 ? change : null));
+        monthTF.setTextFormatter(new TextFormatter<String>(change ->
+                change.getControlNewText().length() <= 2 ? change : null));
+        yearTF.setTextFormatter(new TextFormatter<String>(change ->
+                change.getControlNewText().length() <= 2 ? change : null));
+        CCVTF.setTextFormatter(new TextFormatter<String>(change ->
+                change.getControlNewText().length() <= 3 ? change : null));
+
+        loadUserData();
+    }
+
+    //pre zmenu jazyka -musim este zrobit
+    private void updateText() {
+    }
+
+    //nacitanie info daneho prihlaseneho usera
+    private void loadUserData() {
         User user = Session.getLoggedUser();
+        //info o userovi
         if (user != null) {
             nameText.setText(user.getName());
             surnameText.setText(user.getSurname());
@@ -276,9 +413,20 @@ public class ProfileController {
             if (user.getPhoneNumber() != null) {
                 userPhoneDemo.setText(user.getPhoneNumber());
             }
+
+
+            //toto je na to, ze ked uz ma ulozenu kartu, nech ukazuje rovno edit a nie add
+            if (user.getCardNumber() == null || user.getCardNumber().isEmpty()) {
+                addEditCardButton.setText("Add");
+                deleteCardButton.setVisible(false);
+                noCardLabel.setText("No card");
+            } else {
+                addEditCardButton.setText("Edit");
+                noCardLabel.setText("************" + "/" + "**" + "/" + "**");
+                deleteCardButton.setVisible(true);
+            }
+            isAddingEditingCard = false;
         }
     }
-
-
 
 }
